713ae0a0154cd9462e7046731278adbf4d637eda
DHANANJAY SINGH BHADAURIA <40880864+DhananjayBhadauria@users.noreply.github.com>
2019-11-16 14:26:52 +0530

    login template

--- main_app/models.py
+++ main_app/models.py
@@ -3,23 +3,3 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Institute(models.Model):
-     Name: models.CharField(max_length=255)
-     # attributes here
-
-
-
-
-class Userprofile(models.Model):
-     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-     full_name = models.CharField(max_length=100)
-     # attributes here
-
-
-
-
-
-class Instituteprofile(models.Model):
-     institute = models.OneToOneField(to=Institute, on_delete= models.CASCADE)
-     # attributes here
-
