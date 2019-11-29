3cb9395255ac9f8b9aa965effabda65219056d1c
cportalgh <57786798+cportalgh@users.noreply.github.com>
2019-11-29 16:39:21 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#this is comment
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_portal.settings')
