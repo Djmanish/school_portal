03fd3533597d59e198b8c6b60c5ffb0b10a74bed
DHANANJAY SINGH BHADAURIA <40880864+DhananjayBhadauria@users.noreply.github.com>
2020-01-04 14:24:14 +0530

    fsadk'

--- class_schedule/schedule_signal.py
+++ class_schedule/schedule_signal.py
@@ -2,7 +2,7 @@ from django.dispatch.dispatcher import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import *
-from main_app.models import Institute
+from main_app.models import Institute, Classes
 
 @receiver(post_save, sender=Institute)
 def create_lectures(sender, instance, created, **kwargs):
@@ -13,4 +13,11 @@ def create_lectures(sender, instance, created, **kwargs):
         for stage in class_stages:
             for lecture in lectures:
                 Lecture.objects.create(institute=instance,class_stage= stage, lecture_name= lecture)
+
+# @receiver(post_save, sender=Classes)
+# def create_schedule_days(sender, instance, created, **kwargs):
+#     if created:
+#         days=['Monday','Tuesday', 'Wednesday', 'Thursday','Friday','Saturday']
+#         for day in days:
+#             create_schedule = Schedule.objects.create(institute=instance.institute, Class= instance, day= day )
         
