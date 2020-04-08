8deb9023c66cf867299d81dd25185d6afdb50608
nidhi.gautam8690@gmail.com <neha.gautam869@gmail.com>
2020-04-08 09:10:14 +0530

    update

--- exam_result/urls.py
+++ exam_result/urls.py
@@ -12,6 +12,7 @@ from .views import *
 urlpatterns = [
   
   path('examresult/<int:pk>', views.exam_result, name="examresult"),
+  path('exam_result/<int:pk>',views.examresult, name="exam_result"),
   path('studentview/<int:pk>', views.student_view, name="studentview"),
   path('fetching_sr_no/', views.fetch_sr_no, name='fetch_result_sr_no' ),
   path('report_card/<int:pk>',views.report_card, name='report_card'),
