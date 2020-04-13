8b41b5286bd2783689091757cbf188592439f199
DHANANJAY SINGH BHADAURIA <40880864+DhananjayBhadauria@users.noreply.github.com>
2020-04-13 11:40:46 +0530

    last six days status

--- Attendance/models.py
+++ Attendance/models.py
@@ -8,6 +8,7 @@ from main_app.models import *
 class Attendance(models.Model):
    student = models.ForeignKey(to=User, on_delete=models.CASCADE,null=True, related_name="student_attendance" )
    institute = models.ForeignKey(to=Institute, on_delete=models.CASCADE, related_name='institute_attendance', null=True, blank=True)
+   
    student_class = models.ForeignKey(to=Classes, on_delete=models.CASCADE, related_name="attendance_class", null=True, blank=True) 
    attendance_status = models.CharField(max_length= 10, null = True)
  
