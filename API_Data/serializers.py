847bd42abf65a5564d403f3ba0847ceda1c9e2a3
nidhi.gautam8690@gmail.com <neha.gautam869@gmail.com>
2020-06-27 16:12:09 +0530

    API DATA

--- API_Data/serializers.py
+++ API_Data/serializers.py
@@ -6,20 +6,6 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserDataSerializer(serializers.ModelSerializer):
-   
-    def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super(UserSerializer, self).create(validated_data)
-
-    def update(self, instance, validated_data):
-        if 'user' in validated_data:
-            instance.user.password = make_password(
-                validated_data.get('user').get('password', instance.user.password)
-            )
-            instance.user.save()
     class Meta:
-        model = User
-        fields = ['id','email', 'username', 'password']
-    
-    # validate_password = make_passwords
-    extra_kwargs = {'password': {'write_only': True}}
+        model =User
+        fields= '__all__'
