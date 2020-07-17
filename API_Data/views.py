639c905b8b62887c5b59d9ef62bd9346d6d954c2
nidhi.gautam8690@gmail.com <neha.gautam869@gmail.com>
2020-07-17 14:49:26 +0530

    API Changes

--- API_Data/views.py
+++ API_Data/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from main_app.models import User
+from django.contrib.auth.models import User, AbstractUser
 from API_Data.serializers import UserDataSerializer, RegistrationSerializer
 
 
@@ -16,6 +16,8 @@ from rest_framework import generics
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
 
 @permission_classes((AllowAny, ))
 @api_view(['GET'])
@@ -63,7 +65,7 @@ def registration_view(request):
         data={}
         if serializer.is_valid():
             account = serializer.save()
-            data['response']="Successfully Registered a new account"
+            data['response']="Successfully Registered a new account!"
             data['email']=account.email
             data['username']=account.username
         else:
@@ -87,12 +89,27 @@ class RegisterView(generics.GenericAPIView):
         relativeLink=reverse('email-verify')
         
         absurl='http://'+current_site+relativeLink+"?token="+str(token)
-        email_body='Hi' +user.username+'Use Link below to verify your email\n'+absurl
+        email_body='Hi\n' +user.username+"Use Link below to verify your email\n"+absurl
         data={'email_body':email_body,'to_email':user.email,'email_subject':'Verify Your Email'}
 
         Util.send_email(data)
-        return Response(user_data, status.HTTP_201_CREATED)
+        
+        return Response(user_data)
 
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+    def get(self,request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user= User.objects.get(id=payload['user_id'])
+            if not user.is_staff:
+                user.is_staff = True
+                user.save()
+
+            return Response({'email':'Successfully activated!'}, status=status.HTTP_200_OK)
+
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error':'Activation Expired'},status=status.HTTP_400_BAD_REQUEST)
+
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error':'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
