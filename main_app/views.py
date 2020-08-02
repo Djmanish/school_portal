e46405798ac898d0aa53dcf735f9de0c34443c26
Djmanish <52318902+Djmanish@users.noreply.github.com>
2020-08-03 01:33:06 +0530

    final fix

--- main_app/views.py
+++ main_app/views.py
@@ -296,6 +296,15 @@ def approvals(request,pk):
 def index(request):
     return HttpResponseRedirect(request, 'main_app/index.html')
 
+def get_current_users():
+    active_sessions = Session.objects.filter(expire_date__gte=timezone.now())
+    user_id_list = []
+    for session in active_sessions:
+        data = session.get_decoded()
+        user_id_list.append(data.get('_auth_user_id', None))
+    # Query all logged in users based on id list
+    return User.objects.filter(id__in=user_id_list)
+    
 @login_required
 def dashboard(request):
     # Bus Location
@@ -412,11 +421,7 @@ def dashboard(request):
                 request.user.exam_she_child=ExamDetails.objects.filter(institute=request.user.first_child.child.institute,exam_class=request.user.first_child.child.Class)
             
             if request.method == "POST":
-                request.user.student=request.POST.get('selected_child')
-                std_child=UserProfile.objects.get(id=request.user.student)
-                request.user.post_child=std_child
-                request.user.holiday_child=HolidayList.objects.filter(institute=std_child.institute,applicable="Yes")
-                request.user.exam_she_child=ExamDetails.objects.filter(institute=std_child.institute,exam_class=std_child.Class)
+               
                 if 'calendar' in request.POST:
                     request.user.student=request.POST.get('selected_child')
                     std_child=UserProfile.objects.get(id=request.user.student)
@@ -580,16 +585,16 @@ def dashboard(request):
     # ending student,teacher & class count
     
     # Active Users Count
-    time= timezone.now()- datetime.timedelta(minutes=3)
-    time1= timezone.now()
-    count=UserProfile.objects.filter(user__last_login__gte=time,user__last_login__lte=time1)
     
+    queryset = get_current_users()
+    print("Helloworld")
     online_user=[]
-    for i_user in count:
-        if i_user.institute==request.user.profile.institute:
+    for i_user in queryset:
+        if i_user.profile.institute==request.user.profile.institute:
             online_user.append(i_user)
-
-    len_online_user=len(online_user)
+    for i in online_user:
+        print(i)
+    len_online_user= len(online_user)
     
     # Approvals Data Query Start
     date=datetime.date.today()
@@ -798,6 +803,7 @@ def login(request):
         if request.user.is_authenticated:
             return redirect('user_dashboard')
     except:
+        del request.session['username']
         logout(request)
     if request.method == "POST":
         username = request.POST['username']
