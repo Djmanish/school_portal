b32955de5cc0feef7a66aecbd5125b2113480c7f
cportalgh <dhananjay.bhadauria@trublueappwerks.com>
2020-09-02 09:32:10 +0530

    df

--- main_app/views.py
+++ main_app/views.py
@@ -293,6 +293,7 @@ def approvals(request,pk):
                 Class_teachers = 0
             
             pending_users= UserProfile.objects.filter(status='pending', institute=institute_approval,Class=Class_teachers , designation=student_designation_id).reverse()
+            
             parent_request_inactive= AddChild.objects.filter(status='pending', institute=request.user.profile.institute, Class=Class_teachers)
             parent_request_active= AddChild.objects.filter(status='active', institute=request.user.profile.institute, Class=Class_teachers)
             active_users= UserProfile.objects.filter(status='approve', institute=institute_approval, Class=Class_teachers, designation=student_designation_id).reverse()
